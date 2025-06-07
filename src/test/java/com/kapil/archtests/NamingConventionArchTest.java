5f5a5aab0e528245f57a3757e7575f3581e76a69
kaplimonadi <kapilmonadi@gmail.com>
2025-06-07 21:48:21 +0530

    added test classes

---
+++ src/test/java/com/kapil/archtests/NamingConventionArchTest.java
@@ -0,0 +1,16 @@
+package com.kapil.archtests;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+
+@AnalyzeClasses(packages = "com.kapil")
+public class NamingConventionArchTest {
+
+    @ArchTest
+    ArchRule archRuleNamingConvention = classes().that().
+            resideInAPackage("com.kapil.constants").should().
+            haveSimpleNameEndingWith("Constants");
+}
